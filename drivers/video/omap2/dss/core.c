c10f06a8d911b66f38c88301aedca659b0c03b9e
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-05-17 16:10:59 +0300

    OMAP: DSS2: Init dispc first before other components
    The initialization order of the DSS modules is important when pm_runtime
    support is implemented. Currently RFBI is initialized before DISPC,
    which will cause problems with pm_runtime as RFBI uses DISPC.
    
    Change the init order so that DISPC is before RFBI.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/core.c
+++ drivers/video/omap2/dss/core.c
@@ -185,17 +185,16 @@ static int omap_dss_probe(struct platform_device *pdev)
 
 	/* keep clocks enabled to prevent context saves/restores during init */
 	dss_clk_enable(DSS_CLK_ICK | DSS_CLK_FCK);
+	r = dispc_init_platform_driver();
+	if (r) {
+		DSSERR("Failed to initialize dispc platform driver\n");
+		goto err_dispc;
+	}
 
 	r = rfbi_init_platform_driver();
 	if (r) {
 		DSSERR("Failed to initialize rfbi platform driver\n");
 		goto err_rfbi;
-	}
-
-	r = dispc_init_platform_driver();
-	if (r) {
-		DSSERR("Failed to initialize dispc platform driver\n");
-		goto err_dispc;
 	}
 
 	r = venc_init_platform_driver();
