1a69c2ac1dafa3a82e8a0ba3ddf91de64ee6aa6d
Grant Likely <grant.likely@secretlab.ca>
2011-07-05 23:42:34 -0600

    arm/dt: Add basic devicetree support to IGEPv2 and v3
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/arm/mach-omap2/Makefile.boot
+++ arch/arm/mach-omap2/Makefile.boot
@@ -5,3 +5,5 @@ initrd_phys-y		:= 0x80800000
 dtb-$(CONFIG_MACH_OMAP3_BEAGLE) += omap3-beagle.dtb
 dtb-$(CONFIG_MACH_OMAP4_PANDA) += omap4-panda.dtb
 dtb-$(CONFIG_MACH_OVERO) += omap3-overo.dtb
+dtb-$(CONFIG_MACH_IGEP0020) += isee-igep-v2.dtb
+dtb-$(CONFIG_MACH_IGEP0030) += isee-igep-v3.dtb
--- arch/arm/mach-omap2/board-igep0020.c
+++ arch/arm/mach-omap2/board-igep0020.c
@@ -698,6 +698,11 @@ static void __init igep_init(void)
 	}
 }
 
+static const char *igep2_dt_compat[] __initdata = {
+	"ISEE,igep-v2",
+	NULL
+};
+
 MACHINE_START(IGEP0020, "IGEP v2 board")
 	.boot_params	= 0x80000100,
 	.reserve	= omap_reserve,
@@ -706,7 +711,13 @@ MACHINE_START(IGEP0020, "IGEP v2 board")
 	.init_irq	= omap_init_irq,
 	.init_machine	= igep_init,
 	.timer		= &omap_timer,
+	.dt_compat	= igep2_dt_compat,
 MACHINE_END
+
+static const char *igep3_dt_compat[] __initdata = {
+	"ISEE,igep-v3",
+	NULL
+};
 
 MACHINE_START(IGEP0030, "IGEP OMAP3 module")
 	.boot_params	= 0x80000100,
@@ -716,4 +727,5 @@ MACHINE_START(IGEP0030, "IGEP OMAP3 module")
 	.init_irq	= omap_init_irq,
 	.init_machine	= igep_init,
 	.timer		= &omap_timer,
+	.dt_compat	= &igep3_dt_compat,
 MACHINE_END
