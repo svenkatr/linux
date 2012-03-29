0f1af9138d324261dbf012f064cacfdfe9d7a2cd
Venkatraman S <svenkatr@ti.com>
2012-03-29 22:14:10 +0530

    mmc: core: Add MMC abort interface
    HPI (and possibly other) procedures require that an ongoing
    mmc request issued to a controller be aborted in the middle
    of a transaction. Define a abort interface function to the
    controller so that individual host controllers can safely
    abort a request, stop the dma and cleanup their statemachine
    etc. The implementation is controller dependant
    
    Signed-off-by: Venkatraman S <svenkatr@ti.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -98,6 +98,7 @@ struct mmc_host_ops {
 			    int err);
 	void	(*pre_req)(struct mmc_host *host, struct mmc_request *req,
 			   bool is_first_req);
+	int	(*abort_req)(struct mmc_host *host, struct mmc_request *req);
 	void	(*request)(struct mmc_host *host, struct mmc_request *req);
 	/*
 	 * Avoid calling these three functions too often or in a "fast path",
